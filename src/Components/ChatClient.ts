9f44ecc79f3fa1f569fa2af56408356b0fce0c6d
SujitNasahik <sujitnashik.nashik4@gmail.com>
2023-11-15 17:32:29 -0800

    first commit

---
+++ src/Components/ChatClient.ts
@@ -0,0 +1,87 @@
+import { MessagesContainer, MessageContainer } from "./Globals";
+
+class ChatClient {
+    earliestMessageID: number = 10000000000;
+    previousMessagesFetched: boolean = false;
+    messages: MessageContainer[] = [];
+    updateDisplay: () => void = () => { };
+
+    constructor() {
+        console.log("ChatClient");
+        this.getMessages();
+        this.getMessagesContinuously();
+    }
+
+    setCallback(callback: () => void) {
+        this.updateDisplay = callback;
+    }
+
+    insertMessage(message: MessageContainer) {
+        const messageID = message.id;
+
+        if (this.earliestMessageID > messageID) {
+            this.earliestMessageID = messageID;
+        }
+
+        if (this.messages.length === 0) {
+            this.messages.push(message);
+            return;
+        }
+
+        if (messageID > this.messages[0].id) {
+            this.messages.unshift(message);
+            return;
+        }
+
+        if (messageID < this.messages[this.messages.length - 1].id) {
+            this.messages.push(message);
+            this.previousMessagesFetched = true;
+            return;
+        }
+    }
+
+    insertMessages(messages: MessageContainer[]) {
+        messages.forEach(message => this.insertMessage(message));
+        this.updateDisplay();
+    }
+
+    getMessages(pagingToken: string = '') {
+        const url = `http://localhost:3005/messages/get/`;
+        const fetchURL = `${url}${pagingToken}`;
+
+        fetch(fetchURL)
+            .then(response => response.json())
+            .then((messagesContainer: MessagesContainer) => {
+                this.insertMessages(messagesContainer.messages);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    getMessagesContinuously() {
+        setInterval(() => {
+            this.getMessages();
+        }, 1000);
+    }
+
+    getNextMessages() {
+        const nextMessageToFetch = this.earliestMessageID - 1;
+        const pagingToken = `__${nextMessageToFetch.toString().padStart(10, '0')}__`;
+        this.getMessages(pagingToken);
+    }
+
+    sendMessage(user: string, message: string) {
+        const url = `http://localhost:3005/message/${user}/${message}`;
+        fetch(url)
+            .then(response => response.json())
+            .then((messagesContainer: MessagesContainer) => {
+                this.insertMessages(messagesContainer.messages);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+}
+
+export default ChatClient;
---
+++ src/Components/ChatComponent.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+
+import { MessageContainer } from "./Globals";
+
+import ChatClient from "./ChatClient";
+import './Styles.css'
+import e from "express";
+
+
+
+
+
+const chatClient = new ChatClient();
+
+
+function ChatComponent() {
+    const [messages, setMessages] = useState<MessageContainer[]>([]);
+    const [mostRecentId, setMostRecentId] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(1);  // New state for pagination
+    const messagesPerPage = 20;
+    const [user, setUser] = useState<string>(window.sessionStorage.getItem('userName') || "");
+    // const [user, setUser] = useState<string>("Jose");
+    const [message, setMessage] = useState<string>("Hello World");
+    const bottomRef = useRef(null);
+
+
+    let localUser = user;
+    let localMessage = message;
+    const updateDisplay = useCallback(() => {
+        let updateNeeded = false;
+        const newLastId = chatClient.messages[0].id;
+        if (newLastId !== mostRecentId) {
+            updateNeeded = true;
+        }
+        if (chatClient.previousMessagesFetched) {
+            updateNeeded = true;
+            chatClient.previousMessagesFetched = false;
+        }
+        if (!updateNeeded) {
+            return;
+        }
+    
+        // Correctly slice the messages array
+        const endIndex = messagesPerPage * currentPage;
+        let newMessages = chatClient.messages.slice(0, endIndex);
+    
+        setMessages(newMessages);
+        setMostRecentId(newLastId);
+    }, [mostRecentId, currentPage, messagesPerPage]);
+
+    useEffect(() => {
+        chatClient.setCallback(updateDisplay);
+    }, [updateDisplay]);
+
+
+    function makeFormatedMessages() {
+        let formatedMessages = [...messages].reverse().map((message, index, array) => {
+            if (index === array.length - 1) { // if this is the last message
+                return <textarea key={index} readOnly value={message.id + "]" + message.user + ": " + message.message} ref={bottomRef} />
+            } else {
+                return <textarea key={index} readOnly value={message.id + "]" + message.user + ": " + message.message} />
+            }
+        });
+        return formatedMessages;
+    }
+    function loadMoreMessages() {
+        setCurrentPage(currentPage + 1);  // Increment the current page to load more messages
+    }
+
+    return (
+        <div>
+            <h1>Chat</h1>
+            <button onClick={() => chatClient.getNextMessages()}>Get Messages</button>
+            <div className="scrollable-text-view">
+                {makeFormatedMessages()}
+            </div>
+
+            <input
+                type="text"
+                id="message"
+                placeholder={message}
+                onKeyUp={(event) => {
+                    localMessage = event.currentTarget.value;
+                    setMessage(event.currentTarget.value);
+                    if (event.key === "Enter") {
+                        chatClient.sendMessage(localUser, localMessage);
+                        // clear the message
+                        event.currentTarget.value = "";
+                        setMessage("");
+                    }
+                }}
+            />
+           <button onClick={loadMoreMessages}>Load More Messages</button>  {/* New button to load more messages */}
+            <button onClick={() => chatClient.sendMessage(localUser, localMessage)}>Send</button>
+        </div>
+    );
+}
+
+export default ChatComponent;
